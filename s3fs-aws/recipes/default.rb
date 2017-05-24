b27446c3f83dce37b6a8ad19f02c7640aa667214
Keenan Verbrugge <keenanv@gmail.com>
2017-05-24 10:29:26 -0700

    remove trailing poo from md5sum

--- s3fs-aws/recipes/default.rb
+++ s3fs-aws/recipes/default.rb
@@ -35,7 +35,7 @@ ruby_block "get iam role" do
 	block do
     #tricky way to load this Chef::Mixin::ShellOut utilities
     Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-    command = 'curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d/ -f2 | tr -d \'"\n\''
+    command = 'curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d/ -f2 | tr -d \'",\''
     command_out = shell_out(command)
     node.default['instance_profile'] = command_out.stdout
     end
@@ -46,7 +46,7 @@ ruby_block "get user bucket" do
     block do
         #tricky way to load this Chef::Mixin::ShellOut utilities
         Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
-        command = 'curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d- -f5 | tr -d \'",\' |md5sum'
+        command = 'curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d- -f5 | tr -d \'",\' |md5sum |cut -d ' ' -f1'
         command_out = shell_out(command)
         node.default['user_bucket'] = command_out.stdout
     end
@@ -105,7 +105,7 @@ else
 end
 
 buckets.each do |bucket|
-  directory "#{bucket[:path]}/#{node[:user_bucket]}" do
+  directory bucket[:path] do
     owner     "root"
     group     "root"
     mode      0777
@@ -115,7 +115,7 @@ buckets.each do |bucket|
     end
   end
 		mount bucket[:path] do
-			device "s3fs##{bucket[:name]}/#{node[:user_bucket]}"
+			device "s3fs##{bucket[:name]}:/users/#{node[:user_bucket]}"
 			fstype "fuse"
 			options "iam_role=#{node[:instance_profile]},#{node[:s3fs][:options]}"
 			dump 0
