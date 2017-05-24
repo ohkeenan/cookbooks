c7d15a3a3d847889ebf8e890377713e591ae4ccb
Keenan Verbrugge <keenanv@gmail.com>
2017-05-24 13:16:08 -0700

    try unforcing and simplifying

--- s3fs-aws/recipes/default.rb
+++ s3fs-aws/recipes/default.rb
@@ -33,13 +33,13 @@ end
 
 ruby_block "get iam role" do
 	block do
-    node.default[:s3fs][:iam_role] = 'curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d/ -f2 | tr -d "\","'
+    node.default[:s3fs][:iam_role] = `curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d/ -f2 | tr -d "\","`
     end
 end 
 
 ruby_block 'get user_bucket' do
    block do
-      node.default['user_bucket'] = `curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d- -f5 | tr -d "\"," |md5sum |cut -d " " -f1`
+      node.default[:s3fs][:user_bucket] = `curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d- -f5 | tr -d "\"," |md5sum |cut -d " " -f1`
    end
 end
 
