3e2f45afdda161841f95f3019c1036f032e8f03f
Keenan Verbrugge <keenanv@gmail.com>
2017-05-24 12:39:17 -0700

    move attribute stuff to /attributes

--- s3fs-aws/attributes/default.rb
+++ s3fs-aws/attributes/default.rb
@@ -5,6 +5,29 @@ case node["platform"]
 when "amazon"
   default["s3fs"]["packages"] = %w{gcc libstdc++-devel gcc-c++ curl-devel libxml2-devel openssl-devel mailcap make fuse fuse-devel}
   default["fuse"]["version"] ="2.9.2"
+
+ruby_block "get iam role" do
+    block do
+    #tricky way to load this Chef::Mixin::ShellOut utilities
+    Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+    command = 'curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d/ -f2 | tr -d "\","'
+    command_out = shell_out(command)
+    node.default[:instance_profile] = command_out.stdout                                                                                              
+    end 
+    action :create
+end 
+
+ruby_block "get user bucket" do
+    block do
+        #tricky way to load this Chef::Mixin::ShellOut utilities
+        Chef::Resource::RubyBlock.send(:include, Chef::Mixin::ShellOut)
+        command = 'curl http://169.254.169.254/latest/meta-data/iam/info --silent | grep instance-profile | cut -d- -f5 | tr -d "\"," |md5sum |cut -d " " -f1'
+        command_out = shell_out(command)
+        node.default[:user_bucket] = command_out.stdout
+    end 
+    action :create
+end
+
 end
 
 default["s3fs"]["mount_root"] = '/mnt'
