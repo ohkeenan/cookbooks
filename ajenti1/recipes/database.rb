b7cb2afe9a7f2a05d6310e4d59f5da3ff9df9338
Keenan Verbrugge <keenanv@gmail.com>
2017-05-28 08:06:45 -0700

    nextcloud database

--- ajenti1/recipes/database.rb
+++ ajenti1/recipes/database.rb
@@ -14,39 +14,22 @@ end
 
 vault = chef_vault_item(:credentials, node.name)
 
-node.default['sql_nextcloud']['password'] = vault['sql_nextcloud']
-
-#mysql_connection_info = {
-  #:host     => '127.0.0.1',
-  #:username => 'root'
-  #:password => '' #node['sql_root']['password']
-#}
 mysql_connection_info = {
   :host       => '127.0.0.1',
   :username   => 'root',
   :password   => vault['sql_root']
 }
 
-mysql_database_user 'nextcloud' do
-  connection mysql_connection_info
-  password node['sql_nextcloud']['password']
-  action :create
-end
-
 mysql_database 'nextcloud' do
   connection mysql_connection_info
   action :create
 end
 
-# UPDATE mysql.user SET Password=PASSWORD('#{vault[:sql_root]}') WHERE User='root';
-#  DROP DATABASE IF EXISTS test; FLUSH PRIVILEGES;
-# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-# DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-
-#mysql -uroot -e "\
-#  UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root'; \
-#  DELETE FROM mysql.user WHERE User=''; \
-#  DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); \
-#  DROP DATABASE IF EXISTS test; \
-#  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; \
-#  FLUSH PRIVILEGES;"
+mysql_database_user 'nextcloud' do
+  connection mysql_connection_info
+  password vault['sql_nextcloud']
+  database_name 'nextcloud'
+  host '127.0.0.1'
+  privileges :all
+  action [:create, :grant]
+end
