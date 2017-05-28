df3aa2e0e493bcbc1dbac097e5c546601e601031
Keenan Verbrugge <keenanv@gmail.com>
2017-05-27 21:07:55 -0700

    query going bad try removing set admin pw

--- ajenti1/recipes/database.rb
+++ ajenti1/recipes/database.rb
@@ -30,7 +30,7 @@ mysql_connection_info = {
 mysql_database 'secure_installation' do
   connection mysql_connection_info
   database_name 'mysql'
-  sql "UPDATE mysql.user SET Password=PASSWORD('#{vault['sql_root']}') WHERE User='root'; DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;"
+  sql "DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;"
   action :query
   only_if 'mysql -e "SHOW DATABASES"'
 end
@@ -46,6 +46,7 @@ mysql_database 'nextcloud' do
   action :create
 end
 
+# UPDATE mysql.user SET Password=PASSWORD('#{vault[:sql_root]}') WHERE User='root';
 
 #mysql -uroot -e "\
 #  UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root'; \
