0c4892465bc63a4773159d519008b6666a3b6b7e
Keenan Verbrugge <keenanv@gmail.com>
2017-05-27 21:05:19 -0700

    query going bad try no newlines

--- ajenti1/recipes/database.rb
+++ ajenti1/recipes/database.rb
@@ -30,13 +30,7 @@ mysql_connection_info = {
 mysql_database 'secure_installation' do
   connection mysql_connection_info
   database_name 'mysql'
-  sql "
-  UPDATE mysql.user SET Password=PASSWORD('#{vault['sql_root']}') WHERE User='root'; \
-  DELETE FROM mysql.user WHERE User=''; \
-  DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); \
-  DROP DATABASE IF EXISTS test; \
-  DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; \
-  FLUSH PRIVILEGES;"
+  sql "UPDATE mysql.user SET Password=PASSWORD('#{vault['sql_root']}') WHERE User='root'; DELETE FROM mysql.user WHERE User=''; DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DROP DATABASE IF EXISTS test; DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'; FLUSH PRIVILEGES;"
   action :query
   only_if 'mysql -e "SHOW DATABASES"'
 end
