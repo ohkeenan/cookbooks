4be7f5b84af0eee293aafcb3ac8d246f2dd75fba
Keenan Verbrugge <keenanv@gmail.com>
2017-05-30 14:37:40 -0700

    rainloop download with bsdtar install

---
+++ ajenti1/recipes/rainloop.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: ajenti1
+# Recipe:: rainloop
+#
+# Copyright 2017, Keenan Verbrugge
+#
+#
+
+package ["bsdtar"]
+
+directory "/srv/rainloop" do
+	owner 'www-data'
+	group 'www-data'
+	mode '0750'
+	action :create
+end
+
+execute "download rainloop" do
+	command "wget -qO- https://www.rainloop.net/repository/webmail/rainloop-community-latest.zip | bsdtar -xf- -C /Applications /srv/rainloop/"
+	not_if { ::Dir.exists?("/srv/rainloop/data")}
+end
